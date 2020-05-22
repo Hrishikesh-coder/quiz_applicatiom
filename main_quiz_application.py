721701e80860a6027b7763dd414f7e16830ea88d
Hrishikesh <hrishipotter123@gmail.com>
2020-05-22 22:16:14 +0530

    Made some changes

--- main_quiz_application.py
+++ main_quiz_application.py
@@ -3,3 +3,22 @@ import sqlite3
 db = sqlite3.connect(f'quiz_db.db')
 
 cursor = db.cursor()
+
+def home():
+
+    print("WELCOME TO BHANJA'S QUIZ !! DO YOU WANT TO PLAY A QUIZ ?")
+    answer = input()
+
+    if 'Y' in answer or 'y' in answer:
+        print('''THAT'S GREAT
+        DO YOU HAVE AN ACCOUNT ?''')
+
+        choice = input()
+
+        if 'Y' in choice or 'y' in choice:
+            pass
+        else:
+            pass
+
+    else:
+        pass
